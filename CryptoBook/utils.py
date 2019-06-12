740d877164f92ad1a2d9cf3d25c1e6c453d1b837
Felipe Faria <felipefaria@me.com>
2019-06-11 22:15:23 -0400

    Added async support for historical_data func.

--- CryptoBook/utils.py
+++ CryptoBook/utils.py
@@ -4,7 +4,9 @@ import pandas as pd
 import cfscrape
 import asyncio
 import aiohttp
+
 import ccxt
+import ccxt.async_support as ccxt_async
 
 async def get_ip():
     """ Fetches and returns the external IP of the server for testing purposes. """
@@ -54,31 +56,55 @@ async def historical_data(exchange, symbol, timeframe, start, end, exchange_obje
         str: JSON data with market exchange information.
     """
 
+    # @todo Make sure if exchange_object is passed, it is async.
     if exchange_object:
         ex = exchange_object
         ex.enableRateLimit = False
         ex.session = cfscrape.create_scraper()
     else:
-        ex = getattr (ccxt, exchange) ()
+        ex = getattr (ccxt_async, exchange) ()
 
     # Configuration settings for the DataFrame.
     header = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
     df = pd.DataFrame(columns=header)
 
-    data_start = parse(start)
-    data_end = parse(end)
+    # Setting our start and ending variables as given by the input.
+    time_start = ex.parse8601(parse(start).isoformat())
+    time_end = ex.parse8601(parse(end).isoformat())
 
-    # Re-formats the `start` key to proper ISO8601.
-    current = ex.parse8601(parse(start).isoformat())
+    # Getting the first DataFrame result to measure the size.
+    resp_first = await ex.fetch_ohlcv(symbol, timeframe, time_start)
 
-    # Begins while loop.
-    while data_start < data_end:
-        df = df.append(pd.DataFrame(data = ex.fetch_ohlcv(symbol, timeframe, current), columns=header), ignore_index=True)
+    # The interval of data in which the market is replying by.
+    resp_interval = resp_first[-1][0] - resp_first[0][0]
 
-        current = df[-1:]['Time'].iloc[0]
-        data_start = data_start + timedelta(microseconds=current)
+    # Computes all of the datetime we must request from the exchange.
+    times = []
+    while time_start < time_end:
+        time_start += resp_interval + 60000 # @todo make this dynamic to the interval given.
+        times.append(time_start)
+
+    # Create a DataFrame with the first response.
+    df = df.append(pd.DataFrame(data = resp_first, columns=header), ignore_index=True)
+
+    # Gathers all of the results in order.
+    responses = await asyncio.gather(*[ex.fetch_ohlcv(symbol, timeframe, time) for time in times])
+
+    # Appends all of our results to the DataFrame.
+    dataframes = [pd.DataFrame(data = response, columns=header) for response in responses]
+    df = df.append(dataframes, ignore_index=True)
+
+    # Closes the connection with the exchange (required due to asynchronous support).
+    await ex.close()
+
+    # @todo Ensure that the time is not getting cut-off for other exchanges.
+    # @desc This is due to parse8601 and the manner in which the specific market returns a timestamp. Ensure correct parser is being used.
 
     # Cuts off the DataFrame at the ending time.
-    df = df[df.Time <= ex.parse8601(data_end.isoformat())]
+    df = df[df.Time <= time_end]
 
+    # Removes duplicates due to server responding with more values per call.
+    df = df.drop_duplicates(keep='first')
+
+    # Return the DataFrame as a dictionary.
     return df.to_dict()
