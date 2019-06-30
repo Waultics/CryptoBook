1f86e8f0fe072d070b2cc0532f6e83293f3277e9
Felipe Faria <felipefaria@me.com>
2019-06-29 23:53:10 -0400

    Changed historical examples to be more conservative in time range.

--- examples/Historical/historical-pandas.py
+++ examples/Historical/historical-pandas.py
@@ -6,10 +6,10 @@ import json
 data = {
     "exchange": "binance",
     "symbol": "ETH/BTC",
-    "timeframe": "1m",
+    "timeframe": "1h",
     "start": "2018-01-01 00:00:00",
-    "end": "2019-01-01 00:00:00",
-    "cfbypass": False,  # Optional flag if exchange uses CloudFlare.
+    "end": "2018-01-02 00:00:00",
+    "cfbypass": True,  # Optional flag if exchange uses CloudFlare.
 }
 request_before = datetime.now()
 
@@ -18,19 +18,22 @@ r = requests.post(
     url="http://0.0.0.0:9900/api/v1/cryptobook/historical", data=json.dumps(data)
 )
 
-request_after = datetime.now()
-dataframe_before = datetime.now()
+try:
+    request_after = datetime.now()
+    dataframe_before = datetime.now()
 
-# Reads the request.
-df = pd.read_json(r.text, orient="split")
-# Converts the Time column to DateTime objects.
-df["Time"] = pd.to_datetime(df["Time"], unit="ms")
+    # Reads the request.
+    df = pd.read_json(r.text, orient="split")
+    # Converts the Time column to DateTime objects.
+    df["Time"] = pd.to_datetime(df["Time"], unit="ms")
 
-dataframe_after = datetime.now()
+    dataframe_after = datetime.now()
 
-# Prints the DataFrame.
-print(df.to_string())
+    # Prints the DataFrame.
+    print(df.to_string())
 
-# Printing time resource.
-print("Request time: ", (request_after - request_before).total_seconds())
-print("Dataframe time: ", (dataframe_after - dataframe_before).total_seconds())
+    # Printing time resource.
+    print("Request time: ", (request_after - request_before).total_seconds())
+    print("Dataframe time: ", (dataframe_after - dataframe_before).total_seconds())
+except Exception:
+    print(r.text)
